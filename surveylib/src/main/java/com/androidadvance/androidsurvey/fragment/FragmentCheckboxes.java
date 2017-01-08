518917f464fb67adb682d01dfda6a95682fc23bd
Ashish Jain <gitcloned@gmail.com>
2017-01-08 17:23:30 +0530

    sppc icon

--- surveylib/src/main/java/com/androidadvance/androidsurvey/fragment/FragmentCheckboxes.java
+++ surveylib/src/main/java/com/androidadvance/androidsurvey/fragment/FragmentCheckboxes.java
@@ -8,6 +8,7 @@ import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.CompoundButton;
