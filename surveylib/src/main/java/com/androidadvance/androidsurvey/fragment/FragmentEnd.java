9fd3632c207c424a76d1bce9b9e6c2b7c4c2a23f
Ashish Jain <gitcloned@gmail.com>
2017-02-01 01:26:42 +0530

    formatted end message

--- surveylib/src/main/java/com/androidadvance/androidsurvey/fragment/FragmentEnd.java
+++ surveylib/src/main/java/com/androidadvance/androidsurvey/fragment/FragmentEnd.java
@@ -13,12 +13,16 @@ import android.widget.TextView;
 import com.androidadvance.androidsurvey.Answers;
 import com.androidadvance.androidsurvey.R;
 import com.androidadvance.androidsurvey.SurveyActivity;
+import com.androidadvance.androidsurvey.models.Quote;
+import com.androidadvance.androidsurvey.models.Quotes;
 import com.androidadvance.androidsurvey.models.SurveyProperties;
 
 public class FragmentEnd extends Fragment {
 
     private FragmentActivity mContext;
     private TextView textView_end;
+    private TextView textView_start_quote;
+    private TextView textView_start_quote_by;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -29,7 +33,8 @@ public class FragmentEnd extends Fragment {
 
         Button button_finish = (Button) rootView.findViewById(R.id.button_finish);
         textView_end = (TextView) rootView.findViewById(R.id.textView_end);
-
+        textView_start_quote = (TextView) rootView.findViewById(R.id.textView_end_quote);
+        textView_start_quote_by = (TextView) rootView.findViewById(R.id.textView_end_quote_by);
 
         button_finish.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -51,6 +56,12 @@ public class FragmentEnd extends Fragment {
         SurveyProperties survery_properties = (SurveyProperties) getArguments().getSerializable("survery_properties");
 
         assert survery_properties != null;
+
+        Quote quote = Quotes.getInstance().getRandomQuote();
+
+        textView_start_quote.setText(quote.getQuote());
+        textView_start_quote_by.setText("- " + quote.getAuthor());
+
         textView_end.setText(Html.fromHtml(survery_properties.getEndMessage()));
 
     }
